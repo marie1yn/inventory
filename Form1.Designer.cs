edb1f53fa66653cb1f6e24a521f2d4377f3b7862
Admin <Admin@DESKTOP-TMOBL8C>
2025-02-08 16:29:40 +0800

    updated ui

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            tabPage5 = new TabPage();
+            tabPage6 = new TabPage();
+            imageList1 = new ImageList(components);
+            materialTabControl1.SuspendLayout();
             SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            materialTabControl1.Controls.Add(tabPage1);
+            materialTabControl1.Controls.Add(tabPage2);
+            materialTabControl1.Controls.Add(tabPage3);
+            materialTabControl1.Controls.Add(tabPage4);
+            materialTabControl1.Controls.Add(tabPage5);
+            materialTabControl1.Controls.Add(tabPage6);
+            materialTabControl1.Depth = 0;
+            materialTabControl1.Dock = DockStyle.Fill;
+            materialTabControl1.ImageList = imageList1;
+            materialTabControl1.Location = new Point(3, 64);
+            materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialTabControl1.Multiline = true;
+            materialTabControl1.Name = "materialTabControl1";
+            materialTabControl1.SelectedIndex = 0;
+            materialTabControl1.Size = new Size(1344, 662);
+            materialTabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            tabPage1.ImageKey = "dashboard.png";
+            tabPage1.Location = new Point(4, 39);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1336, 619);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Dashboard";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.ImageKey = "stocks.png";
+            tabPage2.Location = new Point(4, 39);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1336, 619);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Stock Management";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.ImageKey = "suppliers.png";
+            tabPage3.Location = new Point(4, 39);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1336, 619);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Suppliers ";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            tabPage4.ImageKey = "orders.png";
+            tabPage4.Location = new Point(4, 39);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new Size(1336, 619);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Orders & Restocking";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            tabPage5.ImageKey = "data-usage.png";
+            tabPage5.Location = new Point(4, 39);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Size = new Size(1336, 619);
+            tabPage5.TabIndex = 4;
+            tabPage5.Text = "Usage Reports ";
+            tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            tabPage6.ImageKey = "setting.png";
+            tabPage6.Location = new Point(4, 39);
+            tabPage6.Name = "tabPage6";
+            tabPage6.Size = new Size(1336, 619);
+            tabPage6.TabIndex = 5;
+            tabPage6.Text = "Settings";
+            tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "dashboard.png");
+            imageList1.Images.SetKeyName(1, "data-usage.png");
+            imageList1.Images.SetKeyName(2, "orders.png");
+            imageList1.Images.SetKeyName(3, "setting.png");
+            imageList1.Images.SetKeyName(4, "stocks.png");
+            imageList1.Images.SetKeyName(5, "suppliers.png");
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1369, 628);
+            AutoSize = true;
+            ClientSize = new Size(1350, 729);
+            Controls.Add(materialTabControl1);
+            DrawerIsOpen = true;
+            DrawerShowIconsWhenHidden = true;
+            DrawerTabControl = materialTabControl1;
             Name = "Form1";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Inventory Management System";
+            Load += Form1_Load_1;
+            materialTabControl1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TabPage tabPage5;
+        private TabPage tabPage6;
+        private ImageList imageList1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,28 @@
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace inventory
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Red500, Primary.Red600, Primary.Yellow700, Accent.Red400, TextShade.WHITE);
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
